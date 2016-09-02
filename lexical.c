b0606058a51364aea592991112387e9d36d90ef5
dwjones <dwjones@a9721779-b9a4-4edf-a86f-66882cd6156f>
2016-09-02 15:24:01 +0000

    commit broken lexical code
    git-svn-id: https://svn.divms.uiowa.edu/repos/cs4980/jones@16 a9721779-b9a4-4edf-a86f-66882cd6156f

--- lexical.c
+++ lexical.c
@@ -4,4 +4,64 @@
  * Date 8/13/2016 -- existence of file inferred from Lecture 4
  */
 
-/* =BUG= Lecture 4 gives no code, we'll have to write something */
+#include <stdio.h>
+
+#define EXTERN
+#include <lexical.h>
+
+/* the character classifier */
+enum char_type ( OTHER=0, WHITESPACE=1, LETTER=2, DIGIT=4, PUNCTUATION=8 );
+
+/* short forms of character types */
+#define OTH OTHER
+#define WIT WHITESPACE
+#define LET LETTER
+#define DIG DIGIT
+#define PUN PUNCTUATION
+
+/* character classifier table */
+static const chart_type char_class[256] = {
+     /* NUL SOH STX EOT ENQ ACK BEL BS  HT  LF  VT  FF  CR  SO  SI  */
+	OTH,OTH,OTH,OTH,OTH,OTH,OTH,OTH,WIT,WIT,WIT,WIT,WIT,OTH,OTH,
+     /* DLE ....
+        OTH,  ....
+     /*      !   "
+}
+
+#define ISCLASS(ch,class) (char_class[ch]&(class))
+
+static FILE * infile; /* the input file */
+static char ch;       /* the most recent character */
+
+/* =BUG= lex_open needs writing, initializes infile, ch, lex_this, lex_next */
+
+void lex_advance() {
+
+	/* slide the lexical analysis window forward */
+	lex_this = lex_next;
+	
+	while (ISCLASS(ch,WHITESPACE)) {
+		/* skip whitespace */
+		ch = getc( infile );
+		/* =BUG= what if this hits the end of file? */
+		/* =BUG= how do we handle comments? */
+	}
+	if (ISCLASS(ch,DIGIT)) {
+		/* decimal digit */
+		lex_next.type = NUMBER;
+		lex_next.value = 0;
+		do {
+			/* accumulate value of digit */
+			lex_next.value = (lex_next.value * 10)+(ch - '0');
+			/* =BUG= what if there's an overflow? */
+
+			/* get the next digit */
+			ch = getc( infile );
+			/* =BUG= what if this hits the end of file? */
+		} while ((ch >= '0')&&(ch <= '9'));
+	} else {
+		/* =BUG= what about identifiers, strings, punctuation */
+	}
+}
+	}
+}
--- lexical.h
+++ lexical.h
@@ -4,6 +4,10 @@
  * Date 8/13/2016 -- pasted from Lecture 4 with pro-forma improved comments
  */
 
+#ifndef EXTERN
+	#define EXTERN extern
+#endif
+
 enum lex_types { IDENT, KEYWORD, NUMBER, STRING, PUNCT, ENDFILE };
 
 typedef struct lexeme {
@@ -17,3 +21,5 @@ EXTERN lexeme lex_next; /* the next lexeme */
 void lex_open( char * f );
 void lex_advance();
 void lex_put( lexeme * lex, FILE f );
+
+#undef EXTERN
