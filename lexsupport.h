98c4d88b9fca7887fe141cbd36d9ba13b7e865f1
aleinoff <aleinoff@a9721779-b9a4-4edf-a86f-66882cd6156f>
2016-09-22 05:23:34 +0000

    Fixed bug in jones code (I Think)
    Prof. Jones used "LEX_PUC" as a variable name in lexsupport.h. I think he meant
    to use PUNCT, a member of an enum defined in lexical.h
    
    git-svn-id: https://svn.divms.uiowa.edu/repos/cs4980/groupc@158 a9721779-b9a4-4edf-a86f-66882cd6156f

--- lexsupport.h
+++ lexsupport.h
@@ -15,13 +15,13 @@
 
 /* bool lex_ispunc( lexeme lex, punct_type t ); */
 #define lex_ispunc(lex,t) (					\
-	(lex.type == LEX_PUNC) && (lex.value == t)		\
+	(lex.type == PUNCT) && (lex.value == t)		\
 )
 /* returns true if lexeme lex is a punctuation mark of type t */
 
 /* bool lex_ispuncset( lexeme lex, set32_t s ); */
 #define lex_ispuncset(lex,s) (					\
-	(lex.type == LEX_PUNC) && in_set32(lex.value, s)	\
+	(lex.type == PUNCT) && in_set32(lex.value, s)	\
 )
 /* returns true if lexeme lex is a punctuation mark in set t */
 
