d9fbf657c4ec57c52ff5c509b7ff4f77583dabc9
aleinoff <aleinoff@a9721779-b9a4-4edf-a86f-66882cd6156f>
2016-09-25 10:14:27 +0000

    Test all of the error_fatal functions
    TODO need to test the warnings in the error functions.
    
    git-svn-id: https://svn.divms.uiowa.edu/repos/cs4980/groupc@188 a9721779-b9a4-4edf-a86f-66882cd6156f

--- Testing/ErrorTester.cpp
+++ Testing/ErrorTester.cpp
@@ -2,8 +2,25 @@
 #include "../errors.h"
 
 TEST(Errors,fatal) {
-  EXPECT_DEATH(error_fatal(static_cast<error_message>(1),1),  "Fatal error on line");
+  std::cout << "hello test" << std::endl;
+  EXPECT_DEATH(error_fatal(static_cast<error_message>(0),1),  "Cannot open input file.");
+  EXPECT_DEATH(error_fatal(static_cast<error_message>(1),1),  "String pool overflow.");
+  EXPECT_DEATH(error_fatal(static_cast<error_message>(2),1),  "Symbol table overflow");
+  EXPECT_DEATH(error_fatal(static_cast<error_message>(3),1),  "Value too large.");
+  EXPECT_DEATH(error_fatal(static_cast<error_message>(4),1),  "Unclosed string.");
+  EXPECT_DEATH(error_fatal(static_cast<error_message>(5),1),  "Identifier or string too long.");
 }
+
+#if 0
+TEST(Errors, warnings) {
+  EXPECT_DEATH(error_warn(static_cast<error_message>(0),1),  "Cannot open input file.");
+  EXPECT_DEATH(error_warn(static_cast<error_message>(1),1),  "String pool overflow.");
+  EXPECT_DEATH(error_warn(static_cast<error_message>(2),1),  "Symbol table overflow");
+  EXPECT_DEATH(error_warn(static_cast<error_message>(3),1),  "Value too large.");
+  EXPECT_DEATH(error_warn(static_cast<error_message>(4),1),  "Unclosed string.");
+  EXPECT_DEATH(error_warn(static_cast<error_message>(5),1),  "Identifier or string too long.");
+}
+#endif
 
 int main(int argc, char** argv) {
   printf("In main of gtest\n");
