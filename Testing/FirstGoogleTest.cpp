cbdcd060ede3523891272c13f368b46b8ccbf351
aleinoff <aleinoff@a9721779-b9a4-4edf-a86f-66882cd6156f>
2016-09-25 10:14:11 +0000

    Adds a simple GTest
    Soon we should test actual things!
    
    git-svn-id: https://svn.divms.uiowa.edu/repos/cs4980/groupc@181 a9721779-b9a4-4edf-a86f-66882cd6156f

---
+++ Testing/FirstGoogleTest.cpp
@@ -0,0 +1,34 @@
+#include "gtest/gtest.h"
+
+/*
+ * Try out gtest with a simple example
+ * more info at: 
+ * https://github.com/google/googletest/blob/master/googletest/docs/Samples.md
+ * https://github.com/google/googletest
+ */
+
+int Return_Two_Dummy() {
+  return 2;
+}
+
+template<typename TInputType>
+bool Return_Is_Equal(TInputType A, TInputType B) {
+  return A == B;
+}
+
+TEST(Return_Two_Test, DidItWork) {
+  EXPECT_EQ(2, Return_Two_Dummy());
+  EXPECT_EQ(2, Return_Two_Dummy());
+}
+
+TEST(ReturnEqualTest, didItWork) {
+  EXPECT_TRUE(Return_Is_Equal(1,1));
+  EXPECT_FALSE(Return_Is_Equal(2,1));
+}
+
+
+int main(int argc, char** argv) {
+  printf("In main of gtest\n");
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
