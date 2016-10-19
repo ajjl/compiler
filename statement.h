5963c3a336951f9f1fb3c098fd1abd98851ac5ee
hongxli <hongxli@a9721779-b9a4-4edf-a86f-66882cd6156f>
2016-10-19 21:50:50 +0000

    new header file
    git-svn-id: https://svn.divms.uiowa.edu/repos/cs4980/groupc@314 a9721779-b9a4-4edf-a86f-66882cd6156f

---
+++ statement.h
@@ -0,0 +1,33 @@
+fndef EXTERN
+        #define EXTERN extern
+#endif
+
+class Statement {
+public:
+        static Statement * compile( Environment * e );
+        // factory method
+
+        // =BUG= do statements have any attributes?  they are missing here
+};
+
+#undef EXTERN
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+"statement.h" 27L, 701C
