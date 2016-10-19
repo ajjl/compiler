24d83e23c95033d8c9584af68fb3862f9620670c
hongxli <hongxli@a9721779-b9a4-4edf-a86f-66882cd6156f>
2016-10-19 22:02:03 +0000

    new header file of block.cpp
    git-svn-id: https://svn.divms.uiowa.edu/repos/cs4980/groupc@316 a9721779-b9a4-4edf-a86f-66882cd6156f

---
+++ block.h
@@ -0,0 +1,30 @@
+
+// block.h -- interface specificatio for parser/compiler for blocks
+
+// Author: Douglas W. Jones
+// Date 10/13/2016 -- framework established
+
+// Note, the user must include "environment.h"
+// before including this file
+
+// BNF
+// <block> ::= { <block element> [ ; ] }
+// <block element> ::= <declaration> | <statement>
+
+
+
+
+
+
+
+#ifndef EXTERN
+#define EXTERN extern
+#endif
+
+class Block {
+punlic:
+      static Block * compile( Environment * e );
+// factory methid 
+// =BUG= blocks certainly have attributes, they are missing here
+};
+#undef EXTERN
