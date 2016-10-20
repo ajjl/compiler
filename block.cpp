960ffc8335ba094c4fdb0a32b9331f89915d922a
hongxli <hongxli@a9721779-b9a4-4edf-a86f-66882cd6156f>
2016-10-20 17:17:39 +0000

    previous one was a mistake, thankyou Alexander.
    git-svn-id: https://svn.divms.uiowa.edu/repos/cs4980/groupc@329 a9721779-b9a4-4edf-a86f-66882cd6156f

--- block.cpp
+++ block.cpp
@@ -1,67 +1,3 @@
-st login: Wed Oct 19 16:21:41 on ttys000
-HONGXUs-MacBook-Pro:~ hongxuli$ cd jones
-HONGXUs-MacBook-Pro:jones hongxuli$ svn update
-Updating '.':
-U    block.h
-A    statement.cpp
-A    statement.h
-U    errors.c
-U    errors.h
-U    lexical.c
-U    main.cpp
-U    lexsupport.c
-A    declaration.cpp
-U    lexical.h
-U    lexsupport.h
-A    declaration.h
-U    block.cpp
-U    Makefile
-Updated to revision 312.
-HONGXUs-MacBook-Pro:jones hongxuli$ vi statement.cpp
-HONGXUs-MacBook-Pro:jones hongxuli$ vi statement.cpp
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-HONGXUs-MacBook-Pro:jones hongxuli$ vi statement.h
-HONGXUs-MacBook-Pro:jones hongxuli$ ls
-Makefile        declaration.h   lexical.c       program.cpp     stringpool.h
-README          environment.h   lexical.h       program.h       symboltable.c
-block.cpp       errors.c        lexsupport.c    sets.h          symboltable.h
-block.h         errors.h        lexsupport.h    statement.cpp   testlex.c
-config.h        keywords.c      main.cpp        statement.h
-declaration.cpp keywords.h      main.h          stringpool.c
-HONGXUs-MacBook-Pro:jones hongxuli$ vi declaration.cpp
-HONGXUs-MacBook-Pro:jones hongxuli$ vi block.cpp
-
-// block.cpp -- implementation of parser/compiler for blocks
-
-// Author: Douglas W. Jones
-// Date 10/12/2016 -- framework established
-
-// BNF
-// <block> ::= { <block element> [ ; ] }
-// <block element> ::= <declaration> | <statement>
-
 #include <stdint.h>
 #include <stdio.h>
 #include "config.h"
@@ -83,8 +19,8 @@ HONGXUs-MacBook-Pro:jones hongxuli$ vi block.cpp
 // start sets
 #define START_PUNCS SET32_EMPTY
 #define START_KEYS ( to_set32_4( KEY_DO, KEY_IF, KEY_SELECT, KEY_CATCH ) \
-                   | to_set32_3( KEY_RAISE, KEY_WHILE, KEY_FOR )         \
-                   )
+		   | to_set32_3( KEY_RAISE, KEY_WHILE, KEY_FOR )         \
+		   )
 #define START_LEXS to_set32( IDENT )
 
 // follow sets
@@ -96,8 +32,24 @@ HONGXUs-MacBook-Pro:jones hongxuli$ vi block.cpp
 
 Block * Block::compile( Environment * e ) {
 
-        lex_wantinset( START_PUNCS, START_KEYS, START_LEXS, ER_WANT_BLOCK );
+	lex_wantinset( START_PUNCS, START_KEYS, START_LEXS, ER_WANT_BLOCK );
 
-        while (lex_isinset( START_PUNCS, START_KEYS, START_LEXS )) {
+	while (lex_isinset( START_PUNCS, START_KEYS, START_LEXS )) {
 
+		if ( (lex_this.type == IDENT)
+		&&   lex_ispunc( lex_next, PT_COLON ) ) {
+			// all declarations begin with ident:
+			e = Declaration::compile( e );
+		} else {
+			// if not a declaration must be a statement
+			Statement * s = Statement::compile( e );
+		}
+		if (lex_ispunc( lex_this, PT_SEMI )) lex_advance();
+	}
+
+	lex_wantinset( FOLLOW_PUNCS, FOLLOW_KEYS, FOLLOW_LEXS, ER_WANT_ENDBLOK);
+
+	// =BUG= the following is almost certainly an error
+	return NULL;
+}
 
