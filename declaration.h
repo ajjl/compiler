51e581eb4556019b292f118a55c8ed7b0b274e00
bziolko <bziolko@a9721779-b9a4-4edf-a86f-66882cd6156f>
2016-10-21 20:14:36 +0000

    More files I forgot to include.
    git-svn-id: https://svn.divms.uiowa.edu/repos/cs4980/groupc@351 a9721779-b9a4-4edf-a86f-66882cd6156f

---
+++ declaration.h
@@ -0,0 +1,25 @@
+// declaration.h -- interface specificatio for parser/compiler for declarations
+
+// Author: Douglas W. Jones
+// Date 10/15/2016 -- framework established
+//
+// Note, the user must include "environment.h"
+// before including this file
+
+// BNF
+// <declaration> ::= <identifier> : [ <private> | <restricted> ] <declarator>
+
+#ifndef EXTERN
+	#define EXTERN extern
+#endif
+
+class Declaration {
+public:
+	static Environment * compile( Environment * e );
+	// factory method?
+	// =BUG= perhaps Declaration could be a subclass of Environment?
+
+	// =BUG= do declarations have any attributes?  they are missing here
+};
+
+#undef EXTERN
---
+++ keywords.cpp
@@ -0,0 +1,61 @@
+/* keywords.h -- keyword recognition mechanism interface */
+
+/* Author: Douglas W. Jones
+ * Date: 8/23/2016 -- pasted from lecture 14 with pro-forma improved comments
+ */
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "config.h"
+#include "stringpool.h"
+#include "symboltable.h"
+
+#define EXTERN
+#include "keywords.h"
+
+/* list of all the keywords in the language */
+const char * const key_names[] = {
+	"???", /* the null keyword handle */
+	"end",          "const",        "final",        "type",
+	"exception",    "var",          "procedure",    "function",
+	"private",      "restricted",   "external",     "array",
+	"set",          "of",           "record",       "if",
+	"then",         "else",         "select",       "case",
+	"while",        "do",           "until",        "for",
+	"in",           "catch",        "raise",	"enum",
+	"null"
+};
+/* IT IS CRUCIAL that the order of the strings listed in this table
+ * exactly match the order of the keyword names declared in
+ * key_handle in file keywords.h
+ */
+
+/* table of the symbol handles corresponding to each keyword */
+symbol_handle key_table[ KEY_NULL + 1 ];
+
+void key_init() {
+	/* initializer for keyword mechanism */
+        int i;
+        for (i = KEY_INVALID + 1; i <= KEY_NULL; i++) {
+                key_table[i] = symbol_add( key_names[i] );
+        }
+}
+
+void key_put( key_handle k, FILE * f ) {
+	/* output the indicated keyword to the human readable file */
+        fputs( key_names[ k ], f );
+}
+
+key_handle key_lookup( symbol_handle s ) {
+	/* return the keyword handle corresponding to the given symbol handle
+	 * if the symbol is not a keyword, returns KEY_INVALID
+	 */
+	
+        /* =BUG= stupid version, uses a linear search */
+
+        int i = KEY_NULL;
+        while ((i >= KEY_INVALID + 1) && (key_table[i] != s)) i--;
+        return (key_handle)i;
+}
