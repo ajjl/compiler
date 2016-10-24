d98d278aac54a974d22ae2f27a09ee2feec34ac9
hongxli <hongxli@a9721779-b9a4-4edf-a86f-66882cd6156f>
2016-10-24 00:15:48 +0000

    regarding to svn log problem
    git-svn-id: https://svn.divms.uiowa.edu/repos/cs4980/groupc@368 a9721779-b9a4-4edf-a86f-66882cd6156f

--- main.h
+++ main.h
@@ -3,7 +3,7 @@
 /* Author: Douglas W. Jones
  * Date 8/30/2016 -- pasted from Lecture 17 with pro-forma improved comments
  */
-
+/* does anyone know what happend with svn log? I can not update from revision 352, now it is revision 367, it seems not working. Reply me if anyone knows the reason please. Thankyou*/
 /* Prerequisites for use:
  *   In main.c, but nowhere else,
  *     EXTERN must be defined first
