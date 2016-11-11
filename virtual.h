2e616aee01d7d177ef76a6f937b5ca5c61f4521b
hongxli <hongxli@a9721779-b9a4-4edf-a86f-66882cd6156f>
2016-11-11 20:56:28 +0000

    code generation file part
    git-svn-id: https://svn.divms.uiowa.edu/repos/cs4980/groupc@602 a9721779-b9a4-4edf-a86f-66882cd6156f

---
+++ virtual.h
@@ -0,0 +1,12 @@
+//This is a base version of code generation file, continued work is required for matching previous files
+
+#include <tools/c-code.h>
+/*BASIC TYPES*/
+typedef char byte; /*8 BIT*/
+typedef short word; /*16 bit*/
+typedef long 1word; /*32 bit*/
+typedef char *ptr; /*Nominally 32 bit*/
+
+typedef byte array;/* Aliases for "byte"*/
+typedef byte record;
+
