37460e836d45c43d3e12da09a44076f537e44c7d
hongxli <hongxli@a9721779-b9a4-4edf-a86f-66882cd6156f>
2016-11-11 21:02:08 +0000

    code generation header file
    git-svn-id: https://svn.divms.uiowa.edu/repos/cs4980/groupc@603 a9721779-b9a4-4edf-a86f-66882cd6156f

---
+++ c-code.h
@@ -0,0 +1,8 @@
+#define BYTE_WIDTH        1     /* WIDETH OF THE BASIC TYPES*/
+#define WORD_WIDTH        2
+#define LWORD_WIDTH       4
+#define PTR_WIDTH         4
+
+#define BYTE_HIGH_BIT  "0XFF80"   /*HIGH-BIT MASK. */
+#define WORD_HIGH_BIT  "0X8000"
+#define LWORD_HIGH_BIT "0X80000000L"
