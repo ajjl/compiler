12bc2d0502335584abdf795f3007f55ad9b90473
hongxli <hongxli@a9721779-b9a4-4edf-a86f-66882cd6156f>
2016-11-11 21:17:11 +0000

    new define, struct, typedef union class
    git-svn-id: https://svn.divms.uiowa.edu/repos/cs4980/groupc@604 a9721779-b9a4-4edf-a86f-66882cd6156f

--- virtual.h
+++ virtual.h
@@ -10,3 +10,31 @@ typedef char *ptr; /*Nominally 32 bit*/
 typedef byte array;/* Aliases for "byte"*/
 typedef byte record;
 
+#ifdef ALLOC
+#  define I(x)  x
+#  define CLASS /*EMPTY*/
+#endif
+
+struct _words{ word low,high;};
+struct _bytes{ byte b0, b1, b2, b3; };  /* b0 is LSB, b3 is MSB*/
+
+typedef union reg
+{
+   char    *pp   /* pointer     */
+   1word   1;    /*long word    */
+   struct _words  w;   /* two 16-bit words*/
+   struct _bytes b;    /* four 8-bit bytes*/
+   
+}
+reg;
+
+
+CLASS reg r0, r1, r2, r3, r4, r5, r6, r7;  /* REGISTERS*/
+CLASS reg r8, r9, rA, rB, rC, rD, rE, rF; /*end*/
+
+CLASS reg stack [ SDEPYTH ] ; /*run-time stack*/
+CLASS reg *__sp I (= &stack [SDEPTH]);  /* Stack pointer*/
+Class reg  *__fp I (= &stack [SDEPTH]); /* frame pointer*/
+
+
+ 
