deee569bd784c040dbeb07fc33de3b2d34acefbe
hongxli <hongxli@a9721779-b9a4-4edf-a86f-66882cd6156f>
2016-11-11 21:21:39 +0000

    new def code in header file
    git-svn-id: https://svn.divms.uiowa.edu/repos/cs4980/groupc@606 a9721779-b9a4-4edf-a86f-66882cd6156f

--- c-code.h
+++ c-code.h
@@ -5,4 +5,11 @@
 
 #define BYTE_HIGH_BIT  "0XFF80"   /*HIGH-BIT MASK. */
 #define WORD_HIGH_BIT  "0X8000"
+
 #define LWORD_HIGH_BIT "0X80000000L"
+
+
+
+#define SWIDTH  LWORD_WIDTH  /* STACK WIDTH (IN BYTES).  */
+
+#define SDEPTH   1024     /* Number of elements in stack. */
