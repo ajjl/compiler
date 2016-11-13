f1679ddab00c4d6880323af4660ab9a1dbb4419e
hongxli <hongxli@a9721779-b9a4-4edf-a86f-66882cd6156f>
2016-11-13 19:35:01 +0000

    #includes
    git-svn-id: https://svn.divms.uiowa.edu/repos/cs4980/groupc@650 a9721779-b9a4-4edf-a86f-66882cd6156f

---
+++ symtab.cpp
@@ -0,0 +1,18 @@
+
+/*THE MANTAINANCE LAYER: MEMPRY MANAGEMENT FUNCTION*/
+
+
+#include <stdio.h>
+#include<stdlib.h>
+#include<tools/debug.h>
+#include<tools/hash.h>
+#include<tools/1.h>
+#include<tools/compiler.h>
+#include<tools/c-code.h>
+#include "symtab.h"
+#include"value.h"
+#include"proto.h"
+#include"lable.h"
+
+
+
