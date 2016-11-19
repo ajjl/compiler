f0c6f1640af847daf16dfeb473bcb957845a22b0
hongxli <hongxli@a9721779-b9a4-4edf-a86f-66882cd6156f>
2016-11-19 07:27:39 +0000

    MISUNDEERSTANDING, FIXED THE PROBLEM i CREATED.
    git-svn-id: https://svn.divms.uiowa.edu/repos/cs4980/groupc@726 a9721779-b9a4-4edf-a86f-66882cd6156f

--- sets.h
+++ sets.h
@@ -23,40 +23,36 @@ typedef uint32_t set32_t;
 /* the type of sets over domains containing up to 32 elements */
 /* the #ifndef #define #endif structure is for the accuracy of the compiler in case the functions were defined multiple times, should not break the building, ifany thing goes wrong because of this matter, feel free to change it.*/
 
-#ifndef SET32_EMPTY
 #define SET32_EMPTY ((set32_t)0)
-#endif
+
 
 /* set32_t to_set32( int i ) */
-#ifndef to_set32
 #define to_set32(i)   (((set32_t)1)<<(i))
-#endif
+
 /* construct a single-member set32_t value from one integer or enumeration */
 
 /* set32_t to_set32_2( int i, int j ) */
-#ifndef to_set32_2
 #define to_set32_2(i,j)     (to_set32(i) | to_set32(j))
-#endif
+
 /* construct set32_t value from 2 integers */
 
 /* set32_t to_set32_3( int i, int j, int k ) */
-#ifndef to_set32_3
 #define to_set32_3(i,j,k)   (to_set32(i) | to_set32(j) | to_set32(k))
-#endif
+
 /* construct set32_t value from 3 integers */
 
 /* set32_t to_set32_4( int i, int j, int k, int l ) */
-#ifndef to_set32_4
+
 #define to_set32_4(i,j,k,l) (to_set32_2(i,j) | to_set32_2(k,l))
-#endif
+
 
 
 /* test set membership */
 
 /* bool in_set32( int i, set32_t s ) */
-#ifndef in_set32
+
 #define in_set32(i,s) (to_set32(i) & s)
-#endif
+
 
 /* test if integer in set32_t, returns nonzero if so, zero if not */
 
