e98e35d3caecdd434a4cb8c7af41feccc5babdac
hongxli <hongxli@a9721779-b9a4-4edf-a86f-66882cd6156f>
2016-11-19 16:21:57 +0000

    comment out the #undef part as the same reason before in the header file, as same, if raises problem, please tell me and feel free to change it.
    git-svn-id: https://svn.divms.uiowa.edu/repos/cs4980/groupc@731 a9721779-b9a4-4edf-a86f-66882cd6156f

--- block.h
+++ block.h
@@ -25,4 +25,7 @@ private:
         //=BUG= blocks certainly have attributes in private section, they are missing here
 };
 
-#undef EXTERN
+//#undef EXTERN
+/* comment it out, the header file now defines extern which can be used in cpp file*/
+
+
