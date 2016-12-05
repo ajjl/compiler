c9af7e6ec744a81aed509915484b2f49a43a8373
hongxli <hongxli@a9721779-b9a4-4edf-a86f-66882cd6156f>
2016-12-05 05:00:16 +0000

    some notes
    git-svn-id: https://svn.divms.uiowa.edu/repos/cs4980/groupc@1077 a9721779-b9a4-4edf-a86f-66882cd6156f

--- parsetree.cpp
+++ parsetree.cpp
@@ -1,7 +1,16 @@
-/* this is the part generating parse tree from the result of the syntax analyzer,which is also called a parser. */
-
+/* this is the result destination of file program.cpp, block.cpp and all the other files related to the parser, all of the files before to constrcut a parse tree, and the tree lays in this file if nowhere else.*/
 
 /*code start here*/
+
+/*should use linked list to construct a parse tree using the result from other parsing files.*/
+
+
+
+
+
+
+
+
 
 
 
