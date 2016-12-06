cc533e28d97b29c3614943406ea77c73823acfc8
hongxli <hongxli@a9721779-b9a4-4edf-a86f-66882cd6156f>
2016-12-06 07:56:56 +0000

    typeDeclarator.h file
    git-svn-id: https://svn.divms.uiowa.edu/repos/cs4980/groupc@1105 a9721779-b9a4-4edf-a86f-66882cd6156f

---
+++ typeDeclarator.h
@@ -0,0 +1,22 @@
+//this is the type declarator file
+
+//BNF <type declarator> ::= "type" <type>
+//                         |  "type" "-"
+
+
+#ifndef EXTERN 
+#define EXTERN extern
+#endif
+
+class typeDeclarator {
+public:
+        static typeDeclarator * compile( Environment * e );
+ 
+
+      
+private:
+  
+};
+
+#undef EXTERN
+
