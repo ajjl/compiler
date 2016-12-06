f1f02a83400d0a669347f996837d6af3a739ab58
hongxli <hongxli@a9721779-b9a4-4edf-a86f-66882cd6156f>
2016-12-06 08:02:58 +0000

    variableDeclarator.h file
    git-svn-id: https://svn.divms.uiowa.edu/repos/cs4980/groupc@1107 a9721779-b9a4-4edf-a86f-66882cd6156f

---
+++ variableDeclarator.h
@@ -0,0 +1,22 @@
+//variable declarator header file
+
+// bnf <variable declarator> ::= "var" <type>
+
+
+#ifndef EXTERN 
+#define EXTERN extern
+#endif
+
+class Block {
+public:
+        static Block * compile( Environment * e );
+
+
+private:
+
+};
+
+#undef EXTERN
+
+
+
