efe84b403f04493e5bbc4244d477b94a1d830c53
hongxli <hongxli@a9721779-b9a4-4edf-a86f-66882cd6156f>
2016-12-06 07:59:56 +0000

    exceptionDeclarator.h file
    git-svn-id: https://svn.divms.uiowa.edu/repos/cs4980/groupc@1106 a9721779-b9a4-4edf-a86f-66882cd6156f

---
+++ exceptionDeclarator.h
@@ -0,0 +1,21 @@
+//EXCEPTION DECLARATOR HEADER FILE
+
+//bnf <exception declarator> ::= "exception"
+
+
+#ifndef EXTERN 
+#define EXTERN extern
+#endif
+
+lass Block {
+public:
+        static Block * compile( Environment * e );
+
+private:
+
+};
+
+#undef EXTERN
+
+
+
