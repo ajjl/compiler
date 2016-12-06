139b5a196c50ab9a520ac18ed36f40aec0a6c10c
hongxli <hongxli@a9721779-b9a4-4edf-a86f-66882cd6156f>
2016-12-06 08:17:58 +0000

    functionDeclarator.h file
    git-svn-id: https://svn.divms.uiowa.edu/repos/cs4980/groupc@1111 a9721779-b9a4-4edf-a86f-66882cd6156f

---
+++ functionDeclarator.h
@@ -0,0 +1,24 @@
+// functionDeclarator header file    
+// <function declarator> ::=  "function" <type>
+//                            [ <formal parameter list> ]
+//                             <body>
+
+
+
+#ifndef EXTERN 
+#define EXTERN extern
+#endif
+
+class functionDeclarator {
+public:
+        static functionDeclarator * compile( Environment * e );
+
+
+private:
+
+};
+
+#undef EXTERN
+
+
+
