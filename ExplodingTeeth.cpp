7752f9a8216201f4664c46b15fdedb080a70bec0
kntsn <kntsn@a9721779-b9a4-4edf-a86f-66882cd6156f>
2016-12-07 02:59:34 +0000

    in progress code generation outline
    git-svn-id: https://svn.divms.uiowa.edu/repos/cs4980/groupc@1139 a9721779-b9a4-4edf-a86f-66882cd6156f

---
+++ ExplodingTeeth.cpp
@@ -0,0 +1,45 @@
+class AssemblyProclamationOfTheWorkersParadise {
+public:
+
+    // File * output; //Right now we'll just use std::out
+
+
+    void PUSHL(int x){
+
+    };
+    void POPL(int x){
+
+    };
+    void PUSHI( int c){
+
+    };
+    void PUSHGA( int a ) {
+
+    };
+    void LOAD(){
+
+    };
+    void POPS(){
+
+    };
+    void POPSH (){}:
+    void POPSB (){}:
+    void DUP (){}:
+    void ADD (){}:
+    void SUB (){};
+    void MUL (){};
+    void DIV (){};
+    void MOD (){};
+    void GT (){};
+    void LT (){};
+    void GE (){};
+    void LE(){};
+    void EQ(){};
+    void BTRUE (int dst){};
+    void BFALSE (int dst){};
+    void BR (int dst){};
+    void LABEL (int dst){};
+    void PUSHLA (int d){};
+
+
+};
