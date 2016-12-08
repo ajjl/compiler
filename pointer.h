c4b0c2624103ea5f21c9e2ce09fd5bed3d9b865d
hongxli <hongxli@a9721779-b9a4-4edf-a86f-66882cd6156f>
2016-12-08 03:21:34 +0000

    pointer.h file
    git-svn-id: https://svn.divms.uiowa.edu/repos/cs4980/groupc@1164 a9721779-b9a4-4edf-a86f-66882cd6156f

---
+++ pointer.h
@@ -0,0 +1,19 @@
+//<pointer> ::= "@" <type>
+
+#ifndef EXTERN 
+#define EXTERN extern
+#endif
+
+class pointer {
+public:
+   static pointer * compile( Environment * e );
+
+
+
+private:
+
+// nothing lives here yet
+
+};
+
+#undef EXTERN
