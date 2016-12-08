46b92d8a5a26e93a6e248283ff5edc64606bac62
hongxli <hongxli@a9721779-b9a4-4edf-a86f-66882cd6156f>
2016-12-08 03:20:26 +0000

    subrange.h file
    git-svn-id: https://svn.divms.uiowa.edu/repos/cs4980/groupc@1163 a9721779-b9a4-4edf-a86f-66882cd6156f

---
+++ subrange.h
@@ -0,0 +1,19 @@
+//<subrange> ::= <expression> ".." <expression>
+
+#ifndef EXTERN 
+#define EXTERN extern
+#endif
+
+class subrange {
+public:
+   static subrange * compile( Environment * e );
+
+
+
+private:
+
+// nothing lives here yet
+
+};
+
+#undef EXTERN
