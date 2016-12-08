4538f7f365422469fa1642f7cb66a69b42088d2e
hongxli <hongxli@a9721779-b9a4-4edf-a86f-66882cd6156f>
2016-12-08 03:07:44 +0000

    parameter.hfile
    git-svn-id: https://svn.divms.uiowa.edu/repos/cs4980/groupc@1159 a9721779-b9a4-4edf-a86f-66882cd6156f

---
+++ parameter.h
@@ -0,0 +1,20 @@
+//<parameter> ::= <identifier> ":" <parameter declarer>
+
+#ifndef EXTERN 
+#define EXTERN extern
+#endif
+
+class parameter {
+public:
+   static parameter * compile( Environment * e );
+
+
+
+private:
+
+// nothing lives here yet
+
+};
+
+#undef EXTERN
+
