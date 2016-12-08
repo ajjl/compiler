4860efd8d8c67ce6affd08cf319d998afd928167
hongxli <hongxli@a9721779-b9a4-4edf-a86f-66882cd6156f>
2016-12-08 03:23:34 +0000

    array.h file
    git-svn-id: https://svn.divms.uiowa.edu/repos/cs4980/groupc@1165 a9721779-b9a4-4edf-a86f-66882cd6156f

---
+++ array.h
@@ -0,0 +1,21 @@
+//<array> ::= "array" <type> [ "of" ] <type>
+//         |  "array" "of" <type>
+
+
+#ifndef EXTERN 
+#define EXTERN extern
+#endif
+
+class array {
+public:
+   static array * compile( Environment * e );
+
+
+
+private:
+
+// nothing lives here yet
+
+};
+
+#undef EXTERN
