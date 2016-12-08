33747cba65d42b4444c02fa62c4790974495104b
hongxli <hongxli@a9721779-b9a4-4edf-a86f-66882cd6156f>
2016-12-08 03:24:58 +0000

    set.h file
    git-svn-id: https://svn.divms.uiowa.edu/repos/cs4980/groupc@1166 a9721779-b9a4-4edf-a86f-66882cd6156f

---
+++ set.h
@@ -0,0 +1,19 @@
+//<set> ::= "set" [ "of" ] <type>
+
+#ifndef EXTERN 
+#define EXTERN extern
+#endif
+
+class set {
+public:
+   static set * compile( Environment * e );
+
+
+
+private:
+
+// nothing lives here yet
+
+};
+
+#undef EXTERN
